ed84a5b096ac4afad661a846c9b7cc62464ee91d
hfox999 <37228013+hfox999@users.noreply.github.com>
2020-02-11 12:07:29 -0800

    Updated file
    Whoops, should be good now

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import ReactMapGL, {Marker} from "react-map-gl";
+//import * as strandingData from "./data/stranding-data.json";
+
+//you dont need to type myinfo.js since it is default file for import
 
 function App() {
+
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: 700,
+    latitude: 36.954117,
+    longitude: -122.030799,
+    zoom: 13
+  });
+
+  //must return a single JSX element, so wrap in a div
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <ReactMapGL {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/hfox999/ck6crjgkn0bfs1imqs16f84wz"
+      onViewportChange={viewport => {
+        setViewport(viewport);
+      }}  
+      >
+      <Marker
+        latitude={36.974117}
+        longitude= {-122.030792}
+      >
+      
+      </Marker>
+      
+      
+      </ReactMapGL>
+
     </div>
   );
 }
