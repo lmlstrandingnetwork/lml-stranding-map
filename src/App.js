c502af36018add0449025483bb017cfec1676410
Paul Yakovlev <yvpavel@gmail.com>
2020-02-19 23:41:20 -0800

    refactor

--- src/App.js
+++ src/App.js
@@ -1,89 +1,13 @@
-import React, {useState} from 'react';
-import './App.css';
-import ReactMapGL, {Marker, Popup} from "react-map-gl";
-import * as strandings from "./strandings.json";
-//import * as strandingData from "./data/stranding-data.json";
+import React from "react";
+import "./App.css";
+import Map from "./Components/Map";
 
 //you dont need to type myinfo.js since it is default file for import
 
 function App() {
-
-  const [viewport, setViewport] = useState({
-    width: "100%",
-    height: 700,
-    latitude: 36.954117,
-    longitude: -122.030799,
-    zoom: 13
-  });
-
-
-const [selectedStranding, setSelectedStranding] = useState(null);
-
-
   return (
-    
-    /*
-    must return a single JSX element, so wrap in a div
-    */
-    
-
-
     <div className="App">
-
-      <ReactMapGL {...viewport}
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      onViewportChange={viewport => {
-        setViewport(viewport);
-      }}  
-      >
-
-      {strandings.features.map((strand) => (
-        <Marker key = {strand.properties.PARK_ID}
-        latitude = {strand.geometry.coordinates[0]}
-        longitude = {strand.geometry.coordinates[1]}>
-
-        <button 
-          class = "marker-btn" 
-          onClick={(e) => {
-            e.preventDefault();
-            setSelectedStranding(strand); 
-          }}
-        >
-                     
-        <img src = "/seal-face-svgrepo-com.svg" />
-
-        </button>
-
-
-        </Marker>
-
-
-
-
-        ) )}
-
-      {selectedStranding ? (
-        <Popup 
-          latitude= {selectedStranding.geometry.coordinates[0]} 
-          longitude=  {selectedStranding.geometry.coordinates[1]}
-
-          onClose= {() => {
-            setSelectedStranding(null)
-
-        }}
-
-        >
-          <div>
-            <h2> {selectedStranding.properties.NAME} </h2>
-            <p> {selectedStranding.properties.DESCRIPTIO}</p>
-          </div>
-        </Popup>
-        ) : null}
-
-
-      
-      </ReactMapGL>
-
+      <Map />
     </div>
   );
 }
---
+++ src/Components/Map.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "../App.css";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import * as strandings from "../strandings.json";
+
+function Map() {
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: 800,
+    latitude: 36.954117,
+    longitude: -122.030799,
+    zoom: 13
+  });
+
+  const [selectedStranding, setSelectedStranding] = useState(null);
+
+  return (
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={viewport => {
+          setViewport(viewport);
+        }}
+      >
+        {strandings.features.map(strand => (
+          <Marker
+            key={strand.properties.PARK_ID}
+            latitude={strand.geometry.coordinates[0]}
+            longitude={strand.geometry.coordinates[1]}
+          >
+            <button
+              class="marker-btn"
+              onClick={e => {
+                e.preventDefault();
+                setSelectedStranding(strand);
+              }}
+            >
+              <img src="/seal-face-svgrepo-com.svg" alt="seal-face" />
+            </button>
+          </Marker>
+        ))}
+
+        {selectedStranding ? (
+          <Popup
+            latitude={selectedStranding.geometry.coordinates[0]}
+            longitude={selectedStranding.geometry.coordinates[1]}
+            onClose={() => {
+              setSelectedStranding(null);
+            }}
+          >
+            <div>
+              <h2> {selectedStranding.properties.NAME} </h2>
+              <p> {selectedStranding.properties.DESCRIPTIO}</p>
+            </div>
+          </Popup>
+        ) : null}
+      </ReactMapGL>
+    </div>
+  );
+}
+
+export default Map;
