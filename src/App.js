62a7d254513b896c72a44af6792a7a7be9c24610
pauleyashley92 <pauleyashley92@gmail.com>
2020-02-21 14:08:30 -0800

    Very Basic Header added.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import Map from "./Components/Map";
+import Header from "./Components/Header";
+//import { ReactComponent } from "*.svg";
 
 //you dont need to type myinfo.js since it is default file for import
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Map />
     </div>
   );
