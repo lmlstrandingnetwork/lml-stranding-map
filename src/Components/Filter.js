3062eda0dbb57a7fd146b92205880e8904fbbc53
Natalie Wilson <nataliewilson519@gmail.com>
2020-02-25 11:45:59 -0800

    updated filter

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,4 +1,46 @@
-import React from "react";
+import React, { useState } from 'react';
+import Paper from '@material-ui/core/Paper';
+import {
+  FilteringState,
+  IntegratedFiltering,
+} from '@devexpress/dx-react-grid';
+import {
+  Grid,
+  Table,
+  TableHeaderRow,
+  TableFilterRow,
+} from '@devexpress/dx-react-grid-material-ui';
+
+import { generateRows } from '../demo-data/generator';
+
+export default () => {
+  const [columns] = useState([
+    { name: 'name', title: 'Species' },
+    { name: 'gender', title: 'Sex' },
+    { name: 'city', title: 'Year' },
+    //{ name: 'car', title: 'Car' },
+  ]);
+  const [rows] = useState(generateRows({ length: 0 }));
+
+  return (
+    <Paper>
+      <Grid
+        rows={rows}
+        columns={columns}
+      >
+        <FilteringState defaultFilters={[]} />
+        <IntegratedFiltering />
+        <Table />
+        <TableHeaderRow />
+        <TableFilterRow />
+      </Grid>
+    </Paper>
+  );
+};
+
+
+//export default Filter;
+/*import React from "react";
 import {Container, Row, Col } from 'react-grid-system';
 import {
     FilteringState,
@@ -24,4 +66,4 @@ class Filter extends React.Component{
         );
     }
 }
-export default Filter;
+export default Filter;*/
