de44f9ad3a821138dc147bf5e326145ccd8642a6
hfox999 <37228013+hfox999@users.noreply.github.com>
2020-02-25 11:26:45 -0800

    Install React Bootstrap for navbar to work
    Responsive navbar using bootstrap be sure to install it to work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import Map from "./Components/Map";
+import NavMenu from "./Components/NavMenu";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //you dont need to type myinfo.js since it is default file for import
 
 function App() {
   return (
     <div className="App">
-      <Map />
+    	<NavMenu />
+     	<Map />
     </div>
   );
 }
