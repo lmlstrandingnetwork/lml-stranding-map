f2c2cc2cbfa3554fc3379d249c981cea33c4d7cf
Paul Yakovlev <yvpavel@gmail.com>
2020-03-03 10:53:33 -0800

    fetch json from placeholder

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import * as strandings from "../strandings.json";
 
-
 function Map() {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
   const [viewport, setViewport] = useState({
     width: "100%",
     height: 800,
@@ -12,26 +15,30 @@ function Map() {
     longitude: -122.030799,
     zoom: 13
   });
-  
-
-
 
   const [selectedStranding, setSelectedStranding] = useState(null);
+  const [items, setItems] = useState([]);
+
+  const fetchItems = async () => {
+    const data = await fetch(
+      "https://gist.githubusercontent.com/paulyakovlev/03cefd18c257f76efb591b08980cfbf9/raw/bc97314ffa3c77841e27434c108b0f48a6ed8099/dataset.json"
+    );
+
+    const items = await data.json();
+    setItems(items.reports);
+    console.log(items);
+  };
 
   return (
     <div>
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle ="mapbox://styles/hfox999/ck6crjgkn0bfs1imqs16f84wz"
-
+        mapStyle="mapbox://styles/hfox999/ck6crjgkn0bfs1imqs16f84wz"
         onViewportChange={viewport => {
           setViewport(viewport);
         }}
-		
-		
       >
-		
         {strandings.features.map(strand => (
           <Marker
             key={strand.properties.PARK_ID}
@@ -39,13 +46,12 @@ function Map() {
             longitude={strand.geometry.coordinates[1]}
           >
             <button
-              class="marker-btn"
+              className="marker-btn"
               onClick={e => {
                 e.preventDefault();
                 setSelectedStranding(strand);
-				//this.map.flyTo({ center: [-118.4107187, 33.9415889] })
-				//compiles but doesn't do anything?
-				
+                //this.map.flyTo({ center: [-118.4107187, 33.9415889] })
+                //compiles but doesn't do anything?
               }}
             >
               <img src="/seal-face-svgrepo-com.svg" alt="seal-face" />
@@ -63,14 +69,11 @@ function Map() {
           >
             <div>
               <h2> {selectedStranding.properties.SPECIES} </h2>
-             
-				  <p> {selectedStranding.properties.MODIFIED_D} </p>
-                <p> {selectedStranding.properties.DESCRIPTION} </p>
-                <p> {selectedStranding.properties.AGE} </p> 
-                <p> {selectedStranding.properties.SEX} </p>
 
-            
-    
+              <p> {selectedStranding.properties.MODIFIED_D} </p>
+              <p> {selectedStranding.properties.DESCRIPTION} </p>
+              <p> {selectedStranding.properties.AGE} </p>
+              <p> {selectedStranding.properties.SEX} </p>
             </div>
           </Popup>
         ) : null}
