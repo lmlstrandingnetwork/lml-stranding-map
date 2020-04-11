b71f947e8416a82bee8727895ad81191713e1abf
Paul Yakovlev <yvpavel@gmail.com>
2020-04-10 19:00:24 -0700

    refactor, add submit button and focus on species filter

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,96 +1,36 @@
-import React, {PureComponent} from 'react';
+import React from "react";
 import "../App.css";
-import classnames from "classnames";
-import { Container, Row, Col } from "react-grid-system";
-import styles from "./styles.module.css";
 
-export default class Filter extends PureComponent {
+export default class Filter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "Whale, gray" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    console.log("Selected species: " + this.state.value);
+    event.preventDefault();
+  }
   render() {
-   
-    const formClasses = classnames("form-horizontal", styles.form);
-
     return (
       <div className="filter">
-        <form className={formClasses} noValidate>
-          <Container>
-            <Row>
-              <Col>
-                <label className="form-label" htmlFor="price-from">
-                  Species:
-                </label>
-
-                <select
-                  className="form-select"
-                  id="price-from"
-                  placeholder="Choose species"
-                >
-                  <option value="">California Sea Lion (330)</option>
-                  <option value="">Dolphin (14)</option>
-                  <option value="">Beaked Whale(3)</option>
-                </select>
-              </Col>
-              </Row>
-
-              <Row>
-                <Col>
-                  <label className="form-label" htmlFor="postcode">
-                  Sex: 
-                </label>
-                <select className="form-select" id="postcode">
-                  <option value="">Male</option>
-                  <option value="">Female</option>
-                </select>
-                </Col>
-              </Row>
-              
-              <Row>
-                <Col>
-                <label className="form-label" htmlFor="sortorder">
-                  From:
-                </label>
-
-                <select className="form-select" id="sortorder">
-                  <option value="">2010</option>
-                  <option value="">2011</option>
-                  <option value="">2012</option>
-                  <option value="">2013</option>
-                  <option value="">2014</option>
-                  <option value="">2015</option>
-                  <option value="">2016</option>
-                  <option value="">2017</option>
-                  <option value="">2018</option>
-                  <option value="">2019</option>
-                  <option value="">2020</option>
-                </select>
-              </Col>
-              </Row>
-
-              <Row>
-
-              <Col>
-                <label className="form-label" htmlFor="sortorder">
-                  To:
-                </label>
-                <select className="form-select" id="sortorder">
-                  <option value="">2010</option>
-                  <option value="">2011</option>
-                  <option value="">2012</option>
-                  <option value="">2013</option>
-                  <option value="">2014</option>
-                  <option value="">2015</option>
-                  <option value="">2016</option>
-                  <option value="">2017</option>
-                  <option value="">2018</option>
-                  <option value="">2019</option>
-                  <option value="">2020</option>
-                </select>
-              </Col>
-              </Row>
-           
-          </Container>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <label>
+            Species:
+            <select value={this.state.value} onChange={this.handleChange}>
+              <option value="Whale, gray">Whale, gray</option>
+              <option value="Sea lion, California">Seal, California</option>
+              <option value="Porpoise, harbor">Porpoise, harbor</option>
+            </select>
+          </label>
+          <input className="submit-button" type="submit" value="Submit" />
         </form>
       </div>
     );
   }
-
 }
