5967d730eaaa94d1311b289ed1874047ea42a51b
Paul Yakovlev <yvpavel@gmail.com>
2020-04-21 13:16:20 -0700

    deploy firebase add records function

--- functions/index.js
+++ functions/index.js
@@ -1,55 +1,8 @@
-// Import all needed modules.
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import * as algoliasearch from "algoliasearch";
+const functions = require("firebase-functions");
 
-// Set up Firestore.
-admin.initializeApp();
-const db = admin.firestore();
-
-// Set up Algolia.
-// The app id and API key are coming from the cloud functions environment, as we set up in Part 1, Step 3.
-const algoliaClient = algoliasearch(
-  functions.config().algolia.appid,
-  functions.config().algolia.apikey
-);
-// Since I'm using develop and production environments, I'm automatically defining
-// the index name according to which environment is running. functions.config().projectId is a default
-// property set by Cloud Functions.
-const collectionIndexName =
-  functions.config().projectId === "sos-data-viz" ? "strandings" : "strandings";
-const collectionIndex = algoliaClient.initIndex(collectionIndexName);
-
-// Create a HTTP request cloud function.
-export const sendCollectionToAlgolia = functions.https.onRequest(
-  async (req, res) => {
-    // This array will contain all records to be indexed in Algolia.
-    // A record does not need to necessarily contain all properties of the Firestore document,
-    // only the relevant ones.
-    const algoliaRecords: any[] = [];
-
-    // Retrieve all documents from the COLLECTION collection.
-    const querySnapshot = await db.collection("features").get();
-
-    querySnapshot.docs.forEach((doc) => {
-      const document = doc.data();
-      // Essentially, you want your records to contain any information that facilitates search,
-      // display, filtering, or relevance. Otherwise, you can leave it out.
-      const record = {
-        objectID: doc.id,
-        relevantProperty1: document.relevantProperty1,
-        relevantProperty2: document.relevantProperty2,
-        relevantPropertyN: document.relevantPropertyN,
-      };
-
-      algoliaRecords.push(record);
-    });
-
-    // After all records are created, we save them to
-    collectionIndex.saveObjects(algoliaRecords, (_error: any, content: any) => {
-      res
-        .status(200)
-        .send("Features collection was indexed to Algolia successfully.");
-    });
-  }
-);
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,52 @@
-import * as functions from 'firebase-functions';
+// Import all needed modules.
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import algoliasearch from "algoliasearch";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Set up Firestore.
+admin.initializeApp();
+const db = admin.firestore();
+
+// Set up Algolia.
+const algoliaClient = algoliasearch(
+  functions.config().algolia.appid,
+  functions.config().algolia.apikey
+);
+
+// functions.config().projectId is a default property set by Cloud Functions.
+const collectionIndexName = functions.config().projectId;
+const collectionIndex = algoliaClient.initIndex(collectionIndexName);
+
+// Create a HTTP request cloud function.
+export const sendCollectionToAlgolia = functions.https.onRequest(
+  async (req, res) => {
+    // This array will contain all records to be indexed in Algolia.
+    // A record does not need to necessarily contain all properties of the Firestore document,
+    // only the relevant ones.
+    const algoliaRecords: any[] = [];
+
+    // Retrieve all documents from the COLLECTION collection.
+    const querySnapshot = await db.collection("features").get();
+
+    querySnapshot.docs.forEach((doc) => {
+      const document = doc.data();
+      // Essentially, you want your records to contain any information that facilitates search,
+      // display, filtering, or relevance. Otherwise, you can leave it out.
+      // const record = {
+      //   objectID: doc.id,
+      //   relevantProperty1: document.relevantProperty1,
+      //   relevantProperty2: document.relevantProperty2,
+      //   relevantPropertyN: document.relevantPropertyN,
+      //  };
+      // algoliaRecords.push(record);
+      algoliaRecords.push(document);
+    });
+
+    // After all records are created, we save them to
+    collectionIndex.saveObjects(algoliaRecords, (_error: any, content: any) => {
+      res
+        .status(200)
+        .send("Features collection was indexed to Algolia successfully.");
+    });
+  }
+);
