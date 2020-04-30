e2b277edcd2ac3b40fb7c8337ecc9e2342eb98ff
Paul Yakovlev <yvpavel@gmail.com>
2020-04-30 00:25:57 -0700

    move popup to new component, StrandingPopup

---
+++ src/Components/StrandingPopup.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Popup } from "react-map-gl";
+
+const StrandingPopup = (props) => {
+  return (
+    <Popup
+      latitude={props.latitude}
+      longitude={props.longitude}
+      onClose={props.onClose}
+    >
+      <div>
+        <h2> {props.selectedStranding.properties["Common Name"]} </h2>
+        <p> {props.selectedStranding.properties["Date of Examination"]} </p>
+        <p> {props.selectedStranding.properties["Age Class"]} </p>
+        <p> {props.selectedStranding.properties["Sex"]} </p>
+        <p>Latitude: {props.selectedStranding.geometry.coordinates[1]}</p>
+        <p>Longitude: {props.selectedStranding.geometry.coordinates[0]}</p>
+      </div>
+    </Popup>
+  );
+};
+
+export default StrandingPopup;
