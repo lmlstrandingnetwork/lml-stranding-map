2c15e9dfafd59e1f04ff37e1f24b1a9e206ff27b
Aziz Asomiddinov <mi6xino@gmail.com>
2020-05-01 14:57:19 -0700

    import Marker styles into css file

--- src/Components/ClusteredMarkers.js
+++ src/Components/ClusteredMarkers.js
@@ -3,58 +3,59 @@ import React, { useState, useEffect } from "react";
 import MapGL, { Marker, Source, Layer } from "@urbica/react-map-gl";
 import Cluster from "@urbica/react-map-gl-cluster";
 import "mapbox-gl/dist/mapbox-gl.css";
+import "./ClusteredMarkers.css"
 
 const style = {
-    width: "20px",
-    height: "20px",
-    color: "#fff",
-    background: "#1978c8",
-    borderRadius: "20px",
-    textAlign: "center",
-  };
+  width: "20px",
+  height: "20px",
+  color: "#fff",
+  background: "#1978c8",
+  borderRadius: "20px",
+  textAlign: "center",
+};
 
-  const ClusterMarker = ({ longitude, latitude, pointCount }) => (
-    <Marker longitude={longitude} latitude={latitude}>
-      <div style={{ ...style, background: "#f28a25" }}>{pointCount}</div>
-    </Marker>
-  );
+const ClusterMarker = ({ longitude, latitude, pointCount }) => (
+  <Marker longitude={longitude} latitude={latitude}>
+    <div style={{ ...style, background: "#f28a25" }}>{pointCount}</div>
+  </Marker>
+);
 
 const ClusteredMarkers = (props) => {
-    return (
-        <Cluster
-            radius={40}
-            extent={512}
-            nodeSize={64}
-            component={ClusterMarker}
+  return (
+    <Cluster
+      radius={40}
+      extent={512}
+      nodeSize={64}
+      component={ClusterMarker}
+    >
+      {props.strandings.features.map((report) => (
+        <Marker
+          key={report["objectID"]}
+          latitude={report.geometry.coordinates[1]}
+          longitude={report.geometry.coordinates[0]}
+        >
+          <button
+            className="marker-btn"
+            onClick={(e) => {
+              e.preventDefault();
+              props.setSelectedStranding(report);
+            }}
           >
-            {props.strandings.features.map((report) => (
-              <Marker
-                key={report["objectID"]}
-                latitude={report.geometry.coordinates[1]}
-                longitude={report.geometry.coordinates[0]}
-              >
-                <button
-                  className="marker-btn"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    props.setSelectedStranding(report);
-                  }}
-                >
-                  {/* decide which icon to give the animal */}
-                  {report.properties["Common Name"] ===
-                  "Sea lion, California" ? (
-                    <img src="/seal-grey-svgrepo-com.svg" alt="seal-face" />
-                  ) : (
-                    <img src="/red-pin.svg" alt="seal species" />
-                  )}
-                </button>
-              </Marker>
-            ))}
-          </Cluster>
-     
-    );
-  };
+            {/* decide which icon to give the animal */}
+            {report.properties["Common Name"] ===
+              "Sea lion, California" ? (
+                <img src="/seal-grey-svgrepo-com.svg" alt="seal-face" />
+              ) : (
+                <img src="/red-pin.svg" alt="seal species" />
+              )}
+          </button>
+        </Marker>
+      ))}
+    </Cluster>
 
-  export default ClusteredMarkers;
+  );
+};
+
+export default ClusteredMarkers;
 
 
