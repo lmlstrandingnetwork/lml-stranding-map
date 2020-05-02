21e16c6faaa0c3f35d8a250bf96777636b00fb4f
Maya Apotheker <maya4ap@gmail.com>
2020-05-01 17:02:23 -0700

    faq page created

--- src/App.js
+++ src/App.js
@@ -3,13 +3,43 @@ import Filter from "./Components/Filter";
 import NavMenu from "./Components/NavMenu";
 import Footer from "./Components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <NavMenu />
+
+
+      
+      
+
+      <Router>
+
+
+      <Route path="/" exact render={
+		()=>{
+			return (
+			<div>
+		
+  <NavMenu />
       <Filter />
       <Footer />
+			</div>
+			)
+		}
+	}/>
+      <Route path="/FAQ" exact render={
+		()=>{
+			return (
+			<div>
+			
+	<h1> hi</h1>
+			
+			</div>
+			)
+		}
+	}/>
+        </Router>
     </div>
   );
 }
