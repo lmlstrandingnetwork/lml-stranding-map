434324d2e7c1747a196f1a03c45b44169fe4a613
Paul Yakovlev <yvpavel@gmail.com>
2020-05-01 23:10:09 -0700

    center point count

--- src/Components/ClusteredMarkers.js
+++ src/Components/ClusteredMarkers.js
@@ -1,34 +1,38 @@
-
 import React, { useState, useEffect } from "react";
 import MapGL, { Marker, Source, Layer } from "@urbica/react-map-gl";
 import Cluster from "@urbica/react-map-gl-cluster";
 import "mapbox-gl/dist/mapbox-gl.css";
-import "./ClusteredMarkers.css"
-
+import "./ClusteredMarkers.css";
 
 const ClusterMarker = ({ longitude, latitude, pointCount }) => {
   var clusterSize = pointCount * 1.75;
   return (
-    < Marker longitude={longitude} latitude={latitude} >
-      <div style={{
-        height: clusterSize, width: clusterSize, maxHeight: 80, maxWidth: 80, minHeight: 20, minWidth: 20, background: "#087cdb",
-        borderRadius: "75px",
-        textAlign: "center",
-        position: "relative",
-        border: '1px solid #333',
-      }} > {pointCount} </div>
-    </Marker >
+    <Marker longitude={longitude} latitude={latitude}>
+      <div
+        style={{
+          height: clusterSize,
+          width: clusterSize,
+          maxHeight: 80,
+          maxWidth: 80,
+          minHeight: 20,
+          minWidth: 20,
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+          background: "#087cdb",
+          borderRadius: "75px",
+          border: "1px solid #333",
+        }}
+      >
+        {pointCount}
+      </div>
+    </Marker>
   );
 };
 
 const ClusteredMarkers = (props) => {
   return (
-    <Cluster
-      radius={40}
-      extent={512}
-      nodeSize={64}
-      component={ClusterMarker}
-    >
+    <Cluster radius={40} extent={512} nodeSize={64} component={ClusterMarker}>
       {props.strandings.features.map((report) => (
         <Marker
           key={report["objectID"]}
@@ -43,20 +47,16 @@ const ClusteredMarkers = (props) => {
             }}
           >
             {/* decide which icon to give the animal */}
-            {report.properties["Common Name"] ===
-              "Sea lion, California" ? (
-                <img src="/seal-grey-svgrepo-com.svg" alt="seal-face" />
-              ) : (
-                <img src="/red-pin.svg" alt="seal species" />
-              )}
+            {report.properties["Common Name"] === "Sea lion, California" ? (
+              <img src="/seal-grey-svgrepo-com.svg" alt="seal-face" />
+            ) : (
+              <img src="/red-pin.svg" alt="seal species" />
+            )}
           </button>
         </Marker>
       ))}
     </Cluster>
-
   );
 };
 
 export default ClusteredMarkers;
-
-
