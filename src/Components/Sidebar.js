594d25f7e825a1fdc6cf55786808caaeb8a15046
Paul Yakovlev <yvpavel@gmail.com>
2020-05-01 23:54:57 -0700

    initial commit

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -12,18 +12,16 @@ const Sidebar = (props) => {
         setTButtonText={props.toggleButtonText}
       />
       <DropdownRefinementList
-        hoverable
         attribute={"properties.Common Name"}
         limit={50}
         transformItems={(items) => orderBy(items, "label", "asc")}
       />
       <DropdownRefinementList
-        hoverable
         attribute={"properties.Year of Examination"}
         limit={50}
         transformItems={(items) => orderBy(items, "label", "asc")}
       />
-      <DropdownRefinementList hoverable attribute={"properties.Sex"} />
+      <DropdownRefinementList attribute={"properties.Sex"} />
     </div>
   );
 };
