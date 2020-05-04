e2407231fd72bef90150f1eb8637ee98dec5b287
Maya Apotheker <maya4ap@gmail.com>
2020-05-03 17:24:19 -0700

    redirections and new pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Filter from "./Components/Filter";
 import NavMenu from "./Components/NavMenu";
+import About from "./Components/About";
+import Faq from "./Components/faq";
 import Footer from "./Components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
@@ -8,35 +10,31 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-
-
-      
-      
-
       <Router>
-
-
       <Route path="/" exact render={
 		()=>{
 			return (
 			<div>
-		
-  <NavMenu />
-      <Filter />
-      <Footer />
-      
-			</div>
+		  	  <NavMenu />
+      		  <Filter />
+     		  <Footer />
+      		</div>
 			)
 		}
 	}/>
-      <Route path="/FAQ" exact render={
+      <Route path="/faq" exact render={
 		()=>{
 			return (
-			<div>
+			<Faq />
+			)
+		}
+	}/>
+
+<Route path="/about" exact render={
+		()=>{
+			return (
+			<About />
 			
-	<h1> hi</h1>
-			
-			</div>
 			)
 		}
 	}/>
@@ -44,6 +42,5 @@ function App() {
     </div>
   );
 }
-//SWA-65
-//
+
 export default App;
---
+++ src/Components/faq.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavMenu from "./NavMenu";
+
+function Faq() {
+  return (
+     <div>
+       <NavMenu />
+       <h1 text-align="center">Frequently Asked Questions</h1> 
+     </div>
+  );
+}
+
+export default Faq;
