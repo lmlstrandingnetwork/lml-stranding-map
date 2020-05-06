84214236f7d7bb65a66787ae064fa75d87b444d4
Paul Yakovlev <yvpavel@gmail.com>
2020-05-06 16:41:00 -0700

    toggle between show/hide state when button pressed

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -4,10 +4,33 @@ import Map from "./Map";
 import "./Content.css";
 
 const Content = (props) => {
+  const reducer = (isSidebarHidden, action) => {
+    switch (action.type) {
+      case "show":
+        return false;
+      case "hide":
+        return true;
+      default:
+        return isSidebarHidden;
+    }
+  };
+
+  const [isSidebarHidden, dispatch] = React.useReducer(reducer, false);
+
+  const hideSidebar = () => {
+    if (isSidebarHidden === false) {
+      dispatch({ type: "hide" });
+      console.log("show sidebar");
+    } else {
+      dispatch({ type: "show" });
+      console.log("hide sidebar");
+    }
+  };
+
   return (
     <div className="content">
       <div className="info">
-        <SidebarButton />
+        <SidebarButton hideSidebar={hideSidebar} />
         <Stats
           translations={{
             stats(nbHits, timeSpentMS) {
@@ -23,7 +46,7 @@ const Content = (props) => {
 
 const SidebarButton = (props) => {
   return (
-    <button className="sidebar-btn" onClick={props.collapseSidebar}>
+    <button className="sidebar-btn" onClick={props.hideSidebar}>
       <span className="sidebar-btn-tooltip">Hide/show sidebar</span>
     </button>
   );
