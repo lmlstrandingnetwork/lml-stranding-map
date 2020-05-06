98373391cefac4fbdb489e7c89ae41078f35004d
Aziz Asomiddinov <mi6xino@gmail.com>
2020-05-05 17:46:42 -0700

    Experimenting with profile Cards

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,59 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
 
 function About() {
   return (
     <div>
-      <h1 text-align="center">Meet the Team!</h1>
+     <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Harrison Fox</Card.Title>
+          <Card.Text>
+           Harrison Fox Description
+          </Card.Text>
+          <Button variant="primary" href="https://www.linkedin.com">LinkedIn</Button>
+        </Card.Body>
+      </Card>
+
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Pavel Yakovlev</Card.Title>
+          <Card.Text>
+           Harrison Fox Description
+          </Card.Text>
+          <Button variant="primary" href="https://www.linkedin.com">LinkedIn</Button>
+        </Card.Body>
+      </Card>
+
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Harrison Fox</Card.Title>
+          <Card.Text>
+           Harrison Fox Description
+          </Card.Text>
+          <Button variant="primary" href="https://www.linkedin.com">LinkedIn</Button>
+        </Card.Body>
+      </Card>
+
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Harrison Fox</Card.Title>
+          <Card.Text>
+           Harrison Fox Description
+          </Card.Text>
+          <Button variant="primary" href="https://www.linkedin.com">LinkedIn</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
+
+
+
 
 export default About;
