a05040c8b6037b29d36a350f569adc7550635a80
Aziz Asomiddinov <mi6xino@gmail.com>
2020-05-05 18:00:58 -0700

    LinkedIn Badge Experiment

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,54 +1,20 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-
+import {Helmet} from "react-helmet";
 
 function About() {
   return (
     <div>
-     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Harrison Fox</Card.Title>
-          <Card.Text>
-           Harrison Fox Description
-          </Card.Text>
-          <Button variant="primary" href="https://www.linkedin.com">LinkedIn</Button>
-        </Card.Body>
-      </Card>
-
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Pavel Yakovlev</Card.Title>
-          <Card.Text>
-           Harrison Fox Description
-          </Card.Text>
-          <Button variant="primary" href="https://www.linkedin.com">LinkedIn</Button>
-        </Card.Body>
-      </Card>
-
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Harrison Fox</Card.Title>
-          <Card.Text>
-           Harrison Fox Description
-          </Card.Text>
-          <Button variant="primary" href="https://www.linkedin.com">LinkedIn</Button>
-        </Card.Body>
-      </Card>
-
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Harrison Fox</Card.Title>
-          <Card.Text>
-           Harrison Fox Description
-          </Card.Text>
-          <Button variant="primary" href="https://www.linkedin.com">LinkedIn</Button>
-        </Card.Body>
-      </Card>
+      <h1>Meet the Team!</h1>
+      <Helmet>
+        <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+      </Helmet>
+    
+      <div class="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="vertical" data-theme="light" data-vanity="harrison-fox-775323193"><a class="LI-simple-link" href='https://www.linkedin.com/in/harrison-fox-775323193?trk=profile-badge'>Harrison Fox</a></div>
+      <div class="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="vertical" data-theme="light" data-vanity="harrison-fox-775323193"><a class="LI-simple-link" href='https://www.linkedin.com/in/harrison-fox-775323193?trk=profile-badge'>Harrison Fox</a></div>
+      <div class="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="vertical" data-theme="light" data-vanity="azizkhuja"><a class="LI-simple-link" href='https://www.linkedin.com/in/azizkhuja?trk=profile-badge'>Azizkhuja Asomiddinov</a></div>
+      <div class="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="vertical" data-theme="light" data-vanity="azizkhuja"><a class="LI-simple-link" href='https://www.linkedin.com/in/azizkhuja?trk=profile-badge'>Azizkhuja Asomiddinov</a></div>
     </div>
   );
 }
