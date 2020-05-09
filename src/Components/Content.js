a0e88c3b979a325a42f3ee1ae56040016df06585
Paul Yakovlev <yvpavel@gmail.com>
2020-05-09 00:08:32 -0700

    remove random import

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Stats } from "react-instantsearch-dom";
 import Map from "./Map";
 import "./Content.css";
-import { object } from "firebase-functions/lib/providers/storage";
 
 const Content = (props) => {
   const hideSidebar = () => {
