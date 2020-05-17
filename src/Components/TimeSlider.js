5324cfe847ccdce0a16afb7a355c6928549d8c56
Paul Yakovlev <yvpavel@gmail.com>
2020-05-17 14:52:46 -0700

    fix bug where selecting filter after activating time slider crashes the app

--- src/Components/TimeSlider.js
+++ src/Components/TimeSlider.js
@@ -8,12 +8,14 @@ const TimeSlider = (props) => {
   const [endYear, setEndYear] = useState("");
   const [currentYear, setCurrentYear] = useState("");
 
+  // update our current year label and refine by it when slider is moved
   const handleChange = (value) => {
     setValue(value);
-    const selection = [props.items[value].label];
-    props.refine(selection);
+    setCurrentYear(props.items[value].label);
+    props.refine([props.items[value].label]);
   };
 
+  // move the slider on its own on an interval when play button pressed
   const handleClick = () => {
     for (var i = 0; i <= props.items.length - 1; i++) {
       var tick = function (i) {
@@ -26,13 +28,22 @@ const TimeSlider = (props) => {
     }
   };
 
+  // update the range of the slider every time the list of filterable years changes
+  useEffect(() => {
+    const handleFilterChange = () => {
+      setStartYear(props.items[0].label);
+      setEndYear(props.items[props.items.length - 1].label);
+    };
+    handleFilterChange();
+  }, [props.items]);
+
   return (
     <div className={props.className}>
       {props.items.length > 0 ? (
         <div>
-          <h4>{props.items[value].label}</h4>
-          <button className="button" onClick={handleClick} />
-          <label>{props.items[0].label}</label>
+          <h4>{currentYear}</h4>
+          <button className="play-button" onClick={handleClick} />
+          <label>{startYear}</label>
           <input
             type="range"
             min={0}
@@ -41,7 +52,7 @@ const TimeSlider = (props) => {
             value={value}
             onChange={(changeEvent) => handleChange(changeEvent.target.value)}
           />
-          <label>{props.items[props.items.length - 1].label}</label>
+          <label>{endYear}</label>
         </div>
       ) : (
         "Loading..."
