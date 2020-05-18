535d96640ebb56f83e664f1023dce17574d27c81
Paul Yakovlev <yvpavel@gmail.com>
2020-05-17 18:56:14 -0700

    count species in each species group

--- src/Components/Legend.js
+++ src/Components/Legend.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Legend.css";
 import MarkerSVG from "./MarkerSVG";
 import { connectRefinementList } from "react-instantsearch-dom";
 
 const Legend = (props) => {
+  const groupSpecies = (items) => {
+    var counts = items.reduce((p, c) => {
+      var name = c.label.split(",")[0];
+      if (!p.hasOwnProperty(name)) {
+        p[name] = 0;
+      }
+      p[name] += c.count;
+      return p;
+    }, {});
+
+    console.log(counts);
+  };
+
+  useEffect(() => {
+    groupSpecies(props.items);
+  }, [props.items]);
+
   const renderLegendItem = (item, i) => (
     <li key={i}>
       <MarkerSVG markerColor={props.speciesMarkers[item.label.split(",")[0]]} />
