e374257d08173ec04cca2bb62adb490be4bc690e
Paul Yakovlev <yvpavel@gmail.com>
2020-05-18 02:22:20 -0700

    upload raw csv firebase

--- src/Components/UploadPopup.js
+++ src/Components/UploadPopup.js
@@ -1,5 +1,6 @@
 import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
+import csv from "csv";
 import "./UploadPopup.css";
 
 const Popup = (props) => {
@@ -9,6 +10,37 @@ const Popup = (props) => {
 
   const onDrop = useCallback((acceptedFiles) => {
     console.log(acceptedFiles);
+
+    var file = acceptedFiles[0];
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      csv.parse(reader.result, (err, data) => {
+        var featureCollection = [];
+
+        for (var i = 0; i < data.length; i++) {
+          const attribute1 = data[i][0];
+          const attribute2 = data[i][1];
+          const newFeature = {
+            geometry: attribute1,
+            properties: attribute2,
+            type: "Feature",
+          };
+          featureCollection.push(newFeature);
+
+          fetch("FIREBASE_URL", {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newFeature),
+          });
+        }
+      });
+    };
+
+    reader.readAsBinaryString(file);
   }, []);
 
   const {
