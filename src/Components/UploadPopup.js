ccddfd8fff6c474bf14718a9c40bb06a8dee38f3
Paul Yakovlev <yvpavel@gmail.com>
2020-05-19 17:07:05 -0700

    upload data by clicking button

--- src/Components/UploadPopup.js
+++ src/Components/UploadPopup.js
@@ -1,42 +1,47 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import csv from "csv";
 import "./UploadPopup.css";
 
 const Popup = (props) => {
+  const [featureCollection, setFeatureCollection] = useState([]);
+
   const handleClick = () => {
     props.toggle();
   };
 
+  const uploadFeatureCollection = () => {
+    console.log(featureCollection);
+    featureCollection.forEach((feature) => {
+      fetch(process.env.REACT_APP_FIREBASE_DATABASE_URL, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(feature),
+      });
+    });
+  };
+
   const onDrop = useCallback((acceptedFiles) => {
-    console.log(acceptedFiles);
-
     var file = acceptedFiles[0];
-
     const reader = new FileReader();
+
     reader.onload = () => {
       csv.parse(reader.result, (err, data) => {
-        var featureCollection = [];
-
+        var fileContents = [];
         for (var i = 0; i < data.length; i++) {
           const attribute1 = data[i][0];
           const attribute2 = data[i][1];
-          const newFeature = {
+          const item = {
             geometry: attribute1,
             properties: attribute2,
             type: "Feature",
           };
-          featureCollection.push(newFeature);
-
-          fetch("FIREBASE_URL", {
-            method: "POST",
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newFeature),
-          });
+          fileContents.push(item);
         }
+        setFeatureCollection(fileContents);
       });
     };
 
@@ -78,7 +83,12 @@ const Popup = (props) => {
             <h5>Selected file:</h5>
             <ul>{files}</ul>
             {files.length > 0 && (
-              <button className="uploadButton2">Upload</button>
+              <button
+                className="uploadButton2"
+                onClick={uploadFeatureCollection}
+              >
+                Upload
+              </button>
             )}
           </aside>
         </section>
