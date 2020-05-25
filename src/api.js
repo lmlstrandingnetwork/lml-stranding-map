56b11456028b4a0d7f32719865cabe21f664a342
Paul Yakovlev <yvpavel@gmail.com>
2020-05-24 19:13:31 -0700

    fix upload

--- src/api.js
+++ src/api.js
@@ -2,7 +2,5 @@ import axios from "axios";
 
 export default {
   uploadData: (data) =>
-    axios.post(process.env.REACT_APP_FIREBASE_DATABASE_URL, {
-      data,
-    }),
+    axios.post(process.env.REACT_APP_FIREBASE_DATABASE_URL, data),
 };
