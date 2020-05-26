c8a65a96d5b42f6172f1c86af6b5a08384eda8da
Paul Yakovlev <yvpavel@gmail.com>
2020-05-25 18:10:12 -0700

    initial commit, add backend express server

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// create a GET route
+app.get("/express_backend", (req, res) => {
+  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+});
+
+app.post("/firebase_upload", (req, res) => {
+  res.send({ express: "SENDING POST REQUEST TO FIREBASE" });
+});
