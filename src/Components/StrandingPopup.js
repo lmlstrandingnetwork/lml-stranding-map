735b9004c272eae7010a1246277be9b8932087ac
VariableJack <benson@aubenrock.com>
2021-11-04 00:22:03 -0700

    Have the popup be a bit taller, fixed the cases where an undefined date (i.e. it is not given) caused the website to crash. Also started implementing code for acronym conversion

--- src/Components/StrandingPopup.js
+++ src/Components/StrandingPopup.js
@@ -10,65 +10,75 @@ const StrandingPopup = (props) => {
     let header = array.reverse().join(" ");
     return header[0].toUpperCase() + header.substr(1).toLowerCase();
   }
-  const parseDate = (props) => {
-    let date = props.selectedStranding.properties["Date of Examination"];
-    let array = date.split("-");
-    let result = "";
-    result += array[1] + " " + array[2] + ", " + array[0];
-    //result = date;
-    return result;
-  }
-  return (
-    <Popup
-      latitude={props.latitude}
-      longitude={props.longitude}
-      onClose={props.onClose}
-      closeOnClick={true}
-    >
-      <div className="stranding-popup">
-        <div className="bg-image">
-          <h3> {reverseName(props)} </h3>
-        </div>
-        <h2>
-         {props.selectedStranding.properties["Field Number"]}
-        </h2>
-        <p>
-          <span className="highlight"> Examination Date:</span>{" "}
-          {parseDate(props)}{" "}
-        </p>
-        <p>
-          <span className="highlight"> Age Class:</span>{" "}
-          {props.selectedStranding.properties["Age Class"]}{" "}
-        </p>
-        <p>
-          <span className="highlight"> Sex: </span>
-          {props.selectedStranding.properties["Sex"]}{" "}
-        </p>
-        <p>
-          <span className="highlight"> Latitude: </span>
-          {props.selectedStranding.geometry.coordinates[1]}
-        </p>
-        <p>
-          <span className="highlight"> Longitude: </span>
-          {props.selectedStranding.geometry.coordinates[0]}
-        </p>
-        <p>
-          <span className="highlight"> Human Interaction: </span>
-          {
-            props.selectedStranding.properties["Findings of Human Interaction"]
-          }{" "}
-        </p>
-        <p>
-          <span className="highlight"> Condition at Examination: </span>
-          {props.selectedStranding.properties["Condition at Examination"]}{" "}
-        </p>
-        <p>
-          <span className="highlight"> Necropsied Flag: </span>{" "}
-          {props.selectedStranding.properties["Necropsied Flag"]}{" "}
-        </p>
-      </div>
-    </Popup>
-  );
+   const parseDate = (props) => {
+      let date = props.selectedStranding.properties["Date of Examination"];
+      if (date === undefined)
+         return "Not available";
+      let array = date.split("-");
+      let result = "";
+      if (array.length == 3)
+         result == array[1] + " " + array[2] + ", " + array[0];
+      else if (array.length == 2)=
+         result == array[1] + " " + ", " + array[0];
+      else
+         result = array[0];
+      return result;
+   }
+   const humanInter = (props) => {
+      let findings = props.selectedStranding.properties["Findings of Human Interaction"];
+      return findings;
+   }
+   return (
+      <Popup
+         latitude={props.latitude}
+         longitude={props.longitude}
+         onClose={props.onClose}
+         closeOnClick={true}
+      >
+         <div className="stranding-popup">
+         <div className="bg-image">
+            <h3> {reverseName(props)} </h3>
+         </div>
+         <h2>
+            {props.selectedStranding.properties["Field Number"]}
+         </h2>
+         <p>
+            <span className="highlight"> Examination Date:</span>{" "}
+            {parseDate(props)}{" "}
+         </p>
+         <p>
+            <span className="highlight"> Age Class:</span>{" "}
+            {props.selectedStranding.properties["Age Class"]}{" "}
+         </p>
+         <p>
+            <span className="highlight"> Sex: </span>
+            {props.selectedStranding.properties["Sex"]}{" "}
+         </p>
+         <p>
+            <span className="highlight"> Latitude: </span>
+            {props.selectedStranding.geometry.coordinates[1]}
+         </p>
+         <p>
+            <span className="highlight"> Longitude: </span>
+            {props.selectedStranding.geometry.coordinates[0]}
+         </p>
+         <p>
+            <span className="highlight"> Human Interaction: </span>
+            {
+               humanInter(props)
+            }{" "}
+         </p>
+         <p>
+            <span className="highlight"> Condition at Examination: </span>
+            {props.selectedStranding.properties["Condition at Examination"]}{" "}
+         </p>
+         <p>
+            <span className="highlight"> Necropsied Flag: </span>{" "}
+            {props.selectedStranding.properties["Necropsied Flag"]}{" "}
+         </p>
+         </div>
+      </Popup>
+   );
 };
 
 export default StrandingPopup;
