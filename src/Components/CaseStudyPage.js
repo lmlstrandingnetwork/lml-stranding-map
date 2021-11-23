49206bd1edc00dee507336642b47662e774e0cd9
Deeksha Manjunath <deekshamanjunath@Deekshas-MacBook-Air.local>
2021-11-23 15:18:49 -0800

    added prop

--- src/Components/CaseStudyPage.js
+++ src/Components/CaseStudyPage.js
@@ -1,11 +1,26 @@
 import React from "react";
 
-function CaseStudyPage() {
-  return (
-    <div>
-      <h1>Case Study</h1>
-    </div>
-  );
+const CaseStudyPage = (props) => {
+    const [caseStudy, setCaseStudy] = useState([]);
+    // send the parsed records to Firebase through the backend
+    const getCaseStudy = (e) => {
+        e.preventDefault();
+        featureCollection.forEach((element) =>
+          api
+            .uploadData({ record: element, userToken: userToken }, config)
+            .then((response) => {
+              setResponseData(response);
+            })
+            .catch((error) => {
+              console.log(error);
+            })
+        );
+    };
+    return (
+        <div>
+            <h1>Case Study</h1>
+        </div>
+    );
 }
 
 export default CaseStudyPage;
